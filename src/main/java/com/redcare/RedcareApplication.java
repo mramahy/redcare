caece7073dbe238d887ac5bf5bc95dfdf43621f0
muhammadbekette <mramahy@fcih.net>
2025-06-02 12:33:29 +0200

    init redcare project

---
+++ src/main/java/com/redcare/RedcareApplication.java
@@ -0,0 +1,13 @@
+package com.redcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redcare/RedcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
